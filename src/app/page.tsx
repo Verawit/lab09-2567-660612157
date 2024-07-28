ca7b2a10ea05b1ea244f185582344eefb83d9e8d
Verawit <Verawit2548@gmail.com>
2024-07-28 15:28:31 +0700

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import Footer from "@components/Footer";
 import Header from "@components/Header";
 import Task from "@components/Task";
 import TaskInput from "@components/TaskInput";
-
 import { nanoid } from "nanoid";
 import { useState } from "react";
 
